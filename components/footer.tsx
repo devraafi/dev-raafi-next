d481e933282f3b02bab7de3c67f5cb2e7b9142bb
Raafi Muhammad <rafmhmd06@gmail.com>
2020-10-04 12:13:03 +0700

    add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import '../styles/globals.scss'
 
 export default class Footer extends React.Component {
     render() {
         return (
             <div id="footer-dev-raafi">
-
+                <div className="container">
+                    <div className="row justify-content-between">
+                        <div className="col-auto">
+                            <label htmlFor="" className="v-footer rotate-90">2020 *</label>
+                        </div>
+                        <div className="col-auto">
+                            <label htmlFor="" className="v-footer">v _</label>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
